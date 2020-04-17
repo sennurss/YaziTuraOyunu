fd769d2dc5e862911d8bd3be76728a10c92c7762
sennurss <sennur_berber@hotmail.com>
2020-04-17 13:51:22 +0300

    Yazı Tura Oyunu

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace YaziTura
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOyna = new System.Windows.Forms.Button();
+            this.buttonTemizle = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelSonuc = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonOyna
+            // 
+            this.buttonOyna.Location = new System.Drawing.Point(150, 101);
+            this.buttonOyna.Name = "buttonOyna";
+            this.buttonOyna.Size = new System.Drawing.Size(75, 23);
+            this.buttonOyna.TabIndex = 0;
+            this.buttonOyna.Text = "Oyna";
+            this.buttonOyna.UseVisualStyleBackColor = true;
+            this.buttonOyna.Click += new System.EventHandler(this.buttonOyna_Click);
+            // 
+            // buttonTemizle
+            // 
+            this.buttonTemizle.Location = new System.Drawing.Point(237, 101);
+            this.buttonTemizle.Name = "buttonTemizle";
+            this.buttonTemizle.Size = new System.Drawing.Size(75, 23);
+            this.buttonTemizle.TabIndex = 1;
+            this.buttonTemizle.Text = "Temizle";
+            this.buttonTemizle.UseVisualStyleBackColor = true;
+            this.buttonTemizle.Click += new System.EventHandler(this.buttonTemizle_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 109);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(160, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sonuç :";
+            // 
+            // labelSonuc
+            // 
+            this.labelSonuc.AutoSize = true;
+            this.labelSonuc.Location = new System.Drawing.Point(252, 38);
+            this.labelSonuc.Name = "labelSonuc";
+            this.labelSonuc.Size = new System.Drawing.Size(22, 13);
+            this.labelSonuc.TabIndex = 4;
+            this.labelSonuc.Text = "-----";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 136);
+            this.Controls.Add(this.labelSonuc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonTemizle);
+            this.Controls.Add(this.buttonOyna);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOyna;
+        private System.Windows.Forms.Button buttonTemizle;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelSonuc;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YaziTura
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttonTemizle.Enabled = false;
+        }
+
+        private void buttonOyna_Click(object sender, EventArgs e)
+        {
+            Random yaziturauretici = new Random();
+            int yazitura = Convert.ToInt32(yaziturauretici.Next(0, 2));
+            switch (yazitura)
+            {
+                case 0:
+                    pictureBox1.Image = Image.FromFile(@"C:\Users\asus\Desktop\CSharp\Odevler\YaziTura\Yazi.jpg");
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    labelSonuc.Text = "Yazı";
+                    break;
+
+                case 1:
+                    pictureBox1.Image = Image.FromFile(@"C:\Users\asus\Desktop\CSharp\Odevler\YaziTura\Tura.jpg");
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    labelSonuc.Text = "Tura";
+                    break;
+
+
+            }
+
+            buttonOyna.Enabled = false;
+            buttonTemizle.Enabled = true;
+        }
+
+        private void buttonTemizle_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            buttonOyna.Enabled = true;
+            buttonTemizle.Enabled = false;
+        }
+    }
+}
